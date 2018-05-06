e0487b671ed9ac726418cf6f84289da86d204b16
Lakshman <lakshman.kotipalli@outlook.com>
2018-05-06 19:02:01 +0530

    changes

--- app/js/controllers/searchHotelsController.js
+++ app/js/controllers/searchHotelsController.js
@@ -1,3 +1,11 @@
-hotelExplorerApp.controller('searchHotelsCtrl', ['$scope', function ($scope) {
+hotelExplorerApp.controller('searchHotelsCtrl', ['$scope', '$location', function ($scope, $location) {
     $scope.msg = "I love Bharat";
+    document.getElementById('fromDate').valueAsDate = new Date();
+    document.getElementById('toDate').valueAsDate = new Date();
+
+
+    $scope.search = function () {
+        $location.path('/searchResults');
+    };
+
 }]);
