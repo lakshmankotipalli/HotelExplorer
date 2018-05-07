9e933d69661189402c5d740b9fc732fafa4c7108
Lakshman <lakshman.kotipalli@outlook.com>
2018-05-07 08:43:52 +0530

    adding accessibility

--- app/js/controllers/searchHotelsController.js
+++ app/js/controllers/searchHotelsController.js
@@ -2,7 +2,11 @@ hotelExplorerApp.controller('searchHotelsCtrl', ['$scope', '$location', function
     $scope.msg = "I love Bharat";
     document.getElementById('fromDate').valueAsDate = new Date();
     document.getElementById('toDate').valueAsDate = new Date();
-
+    // document.onkeydown = function(e) {
+    //     if(e.keyCode === 13) { // The Enter/Return key
+    //         document.activeElement.onclick(e);
+    //     }
+    // };
 
     $scope.search = function () {
         $location.path('/searchResults');
